a30d1fceb7b5ce4258ff052fc5cd7f3c7f0e01de
Juan Gabriel Gutierrez <correo.del.llanox@gmail.com>
2015-09-02 17:25:51 -0500

    moving from rebound library to Animated

--- helpers/DraggableDrawerHelper.js
+++ helpers/DraggableDrawerHelper.js
@@ -2,15 +2,17 @@
 'use strict'
 
 var rebound = require('rebound');
+var Animated = require('Animated');
 
 
 module.exports = function (screen_height ) {
     var module = {};
     var initialUsedSpace;
     var tension;
-    var _scrollSpring;
-    var _springSystem;
+    var friction;
     var initialPosition;
+
+    var callbackPositionUpdated;
 
     
      module.calculateInitialPosition = function ( initial_used_space ){
@@ -31,58 +33,40 @@ module.exports = function (screen_height ) {
     module.setupAnimation = function ( higher_tension, friction, callbackPositionUpdated ){
            
 
-          tension = higher_tension;
-           // Initialize the spring that will drive animations
-          _springSystem = new rebound.SpringSystem();
-          _scrollSpring = _springSystem.createSpring();
-          var springConfig = _scrollSpring.getSpringConfig();
-          springConfig.tension = tension;
-          springConfig.friction = friction;
+          this.tension = higher_tension;
+          this.friction = friction;
+          this.callbackPositionUpdated = callbackPositionUpdated; 
 
-          _scrollSpring.setCurrentValue(0);
-
-          _scrollSpring.addListener({
-             onSpringUpdate: () => {
-
-               var currentValue = _scrollSpring.getCurrentValue();
-               var newPosition =  screen_height * currentValue;
-
-                 callbackPositionUpdated && callbackPositionUpdated(newPosition);
-              
-             },
-         });
-
-
-          _scrollSpring.setCurrentValue( 1 - initialUsedSpace );
-
-
-
-      };
+       };
 
 
       module.isAValidMovement = function(distanceX, distanceY){
 
-        var moveTravelledFarEnough =  Math.abs(distanceY) > Math.abs(distanceX) && Math.abs(distanceY) > 2;
+         var moveTravelledFarEnough =  Math.abs(distanceY) > Math.abs(distanceX) && Math.abs(distanceY) > 2;
 
         return moveTravelledFarEnough;
 
       };
 
 
-      module.startAnimation = function (velocityY, positionY ){
-
-     
+      module.startAnimation = function (velocityY, positionY ){     
 
 
          var isGoingToUp = ( velocityY < 0 )? true : false;   
          var speed = Math.abs(velocityY);    
          var currentPosition = Math.abs(positionY / screen_height); 
-         var endPosition = isGoingToUp? 0 : 1 - initialUsedSpace;  
-         _scrollSpring.tension = tension * speed;
-         _scrollSpring.setCurrentValue(currentPosition); 
-         _scrollSpring.setVelocity(speed).setEndValue(endPosition) ; 
-     
+         var endPosition = isGoingToUp? 0 : 1 - initialUsedSpace; 
 
+         var position = new Animated.Value(currentPosition);
+         
+         Animated.spring(position, {toValue: endPosition, 
+                                   velocity: speed, 
+                                   tension:this.tension, 
+                                   friction: this.friction 
+                                   }
+          ).start(this.callbackPositionUpdated);
+
+      
       };
 
    
